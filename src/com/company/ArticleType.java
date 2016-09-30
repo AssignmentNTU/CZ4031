9a58cc2ba265ae9c133bed7a48bc4154719684cd
Edward Sujono <edwardsujono@yahoo.com>
2016-10-01 00:32:38 +0800

    added

---
+++ src/com/company/ArticleType.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by LENOVO on 26/09/2016.
+ */
+public class ArticleType {
+
+    public static enum TYPE{
+        ARTICLE,PHD_THESIS,BOOK,PROCEEDINGS,WEBSITE,IN_COLLECTION,MASTER_THESIS,IN_PROCEEDINGS
+    }
+
+}
